4c95539b499548813da5f8f9cc95e886ad14f944
jimmy sebastian higa ramirez <sebascursoshtml@gmail.com>
2022-10-05 17:52:51 -0300

    footer Create

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Footer from './footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Footer/>
     </div>
   );
 }
---
+++ src/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./footer.css";
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className='container-footer'>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
